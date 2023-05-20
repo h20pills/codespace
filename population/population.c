ea9308af5e448c4887605b98d348ecf7d0255e12
h20pills <133925183+h20pills@users.noreply.github.com>
2023-05-20 20:44:28 +0000

    Sat, May 20, 2023, 8:44 PM +00:00

--- population/population.c
+++ population/population.c
@@ -3,11 +3,7 @@
 
 int main(void)
 {
-    // TODO: Prompt for 100
-
-    // TODO: Prompt for 200
-
-    // TODO: Calculate number of years until we reach threshold
-
-    // TODO: Print number 9
+    start size: 
+    end size:
+    years:
 }
