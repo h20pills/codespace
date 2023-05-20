485ba613bd8edab51db0d173d929798e74b4a385
h20pills <133925183+h20pills@users.noreply.github.com>
2023-05-20 21:40:04 +0000

    Sat, May 20, 2023, 9:40 PM +00:00

--- population/population.c
+++ population/population.c
@@ -4,11 +4,11 @@
 int main(void)
 {
     // Prompt user for start size
-    int start size = get_int("start size: ");
+    int startsize = get_int("startsize: ");
 
     // Prompt user for end size
     int y = get_int("y: ");
 
     // Perform calculation
-    printf("%i\n", x + y);
+    printf("%i\n", startsize + y);
 }
