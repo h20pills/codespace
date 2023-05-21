3f763ce35d12a19947b8d6be5a525d3ac5487768
h20pills <133925183+h20pills@users.noreply.github.com>
2023-05-21 01:07:07 +0000

    Sun, May 21, 2023, 1:07 AM +00:00

--- population/population.c
+++ population/population.c
@@ -6,13 +6,14 @@ int main(void)
 
     // Prompt user for start size
     int n = get_int("Start size: ");
-while (n < 9)
+while (n < 9);
 {
     get_int("Start size: ");
-}
-   else if (n < 9) // Prompt user for end size
-   { int y = get_int("End size: ");
+
+else if (n < 9);
+    {int y = get_int("End size: ");
    }
+}
 
     // Perform calculation
     printf("Years: %i", (n / 3) - (n / 4) + n);
